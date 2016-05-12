59b71fecbb24fbad0b2477a76620782087468d82
Nicola Tomassoni <nicola@digisin.it>
2016-05-12 12:55:51 +0200

    Check names

--- src/Ldap.php
+++ src/Ldap.php
@@ -1,9 +1,10 @@
 <?php
-namespace Edvlerblog;
+
+namespace nonzod;
 
 /**
  * YII2 wrapper class for the Zend Ldap Module.
- * Look at https://packagist.org/packages/adldap/adldap for the Adldap Module
+ * Look at https://packagist.org/packages/adldap/adldap for the zend-ldap Module
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -15,54 +16,51 @@ namespace Edvlerblog;
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  */
-
-
 use yii\base\Component; //include YII component class
 use Zend\Ldap\Ldap;
 
-class Ldap extends Component
-{
-    /**
-     * The internal Adldap object.
-     *
-     * @var object Adldap
-     */
-    private $ZendLdapClass;
+class Ldap extends Component {
 
-    /**
-     * Options variable for the Adldap module.
-     * See Adldap __construct function for possible values.
-     *
-     * @var array Array with option values
-     */
-    public $options = [];
+  /**
+   * The internal zend-ldap object.
+   *
+   * @var object zend-ldap
+   */
+  private $ZendLdapClass;
 
-    /**
-     * init() called by yii.
-     */
-    public function init()
-    {
+  /**
+   * Options variable for the zend-ldap module.
+   * See zend-ldap __construct function for possible values.
+   *
+   * @var array Array with option values
+   */
+  public $options = [];
+
+  /**
+   * init() called by yii.
+   */
+  public function init() {
 //        try {
-            $this->ZendLdapClass = new Ldap($this->options);
+    $this->ZendLdapClass = new Ldap($this->options);
 //        } catch (Exception $e) {
 //            throw $e;
 //        }
-    }
+  }
 
-    /**
-     * Use magic PHP function __call to route function calls to the Adldap class.
-     * Look into the Adldap class for possible functions.
-     *
-     * @param string $methodName Method name from Adldap class
-     * @param array $methodParams Parameters pass to method
-     * @return mixed
-     */
-    public function __call($methodName, $methodParams)
-    {
-        if (method_exists($this->ZendLdapClass, $methodName)) {
-            return call_user_func_array(array($this->ZendLdapClass, $methodName), $methodParams);
-        } else {
-            return parent::__call($methodName, $methodParams);
-        }
+  /**
+   * Use magic PHP function __call to route function calls to the zend-ldap class.
+   * Look into the zend-ldap class for possible functions.
+   *
+   * @param string $methodName Method name from zend-ldap class
+   * @param array $methodParams Parameters pass to method
+   * @return mixed
+   */
+  public function __call($methodName, $methodParams) {
+    if (method_exists($this->ZendLdapClass, $methodName)) {
+      return call_user_func_array(array($this->ZendLdapClass, $methodName), $methodParams);
+    } else {
+      return parent::__call($methodName, $methodParams);
     }
+  }
+
 }
